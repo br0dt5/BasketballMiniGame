04d44d6a7da1d3f1761084edcc9a4934d4340fad
Luan Zaniqueli <luanzaniqueli.carlos@yahoo.com.br>
2022-08-29 20:15:09 -0300

    Commit inicial

---
+++ Assets/Project/Scripts/Ball.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    public GameObject trailObject;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        trailObject.SetActive(false);
+    }
+
+    public void ActivateTrail()
+    {
+        trailObject.SetActive(true);
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Descrição
+Projeto criado para fins de estudo sobre o desenvolvimento de jogos através da ferramenta Unity, usando de base os tutoriais oferecidos pela Zenva Academy.
